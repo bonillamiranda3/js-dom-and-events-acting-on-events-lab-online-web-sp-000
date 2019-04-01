d151483ff4bf5137f1766d4132e5fb3278c544aa
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-04-01 04:10:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,12 +18,16 @@ function addNewElementAsLi() {
 }
 
 
+
+
+
+
 function addNewLiOnClick() {
-  let submit = document.querySelector('input'[type='submit'])
-  submit.addEventListener('click', function(event) {
-    addNewElementAsLi();
-    resetInput();
-  });
+  let submit = document.querySelector('input[type="submit"]')
+    submit.addEventListener('click', function(event){
+      addNewElementAsLi()
+      input.value = ''
+    })
 }
 
 
