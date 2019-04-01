4f9e85915bebeba8ad4350a20e8ad6a475de22e5
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-04-01 03:57:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,29 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+  return document.querySelector('input').value;
+}
+
+function addNewElementAsLi() {
+  let ul = document.querySelector('employee-list');
+  let li = document.createElement('li');
+  li.innerHTML = retrieveEmployeeInformation();
+  ul.appendChild(li);
+}
+
+function addNewLiOnClick() {
+  let submit = document.querySelector('input'[type='submit'])
+  submit.addEventListener('click', function(event) {
+    addNewElementAsLi();
+    resetInput();
+  });
+}
+
+functionn clearEmployeeListOnLinkClick() {
+  document.querySelector('a').addEventListener('click', function(event) {
+    let ul = document.querySelector('.employee-list')
+    ul.innerHTML = ''
+  });
+}
